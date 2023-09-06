8ebad75fdc27da77421ca78a9419db83375a4142
escotoj <j.escoto14@gmail.com>
2023-09-05 17:33:42 -0700

    speedrun of leetcode 1 using functional expresion and arrow

--- 1.js
+++ 1.js
@@ -11,20 +11,68 @@
 
 let nums = [2, 7, 11, 15, 11, 15, 12, 2, 12, 3, 4, 7, 12, 20];
 let target = 24;
-function leetCodeOne(nums, target) {
-    const numberMap = new Map();
-for (let index = 0; index < nums.length; index++) {
-    const difference = target - nums[index];
-if (numberMap.has(difference)) {
-    return [numberMap.get(difference), index]
+// function leetCodeOne(nums, target) {
+//     const numberMap = new Map();
+// for (let index = 0; index < nums.length; index++) {
+//     const difference = target - nums[index];
+// if (numberMap.has(difference)) {
+//     return [numberMap.get(difference), index]
+// }
+// numberMap.set(nums[index], index)   
+// }
+
+// return [];
+
+// }
+
+// let result = leetCodeOne(nums, target)
+
+// console.log('indeces', result)
+
+
+// function twoSum (nums, target) {
+//     const numMap = new Map();
+//     for (let i = 0; i < nums.length; i++) {
+//         const diff = target - nums[i];
+//         if (numMap.has(diff)) {
+//             return [numMap.get(diff), i]
+//         } 
+//         numMap.set(nums[i], i)
+//     }
+//     // console.log(numMap)
+//     return []
+
+// } 
+
+// const results = twoSum(nums, target)
+// console.log(results)
+
+// const twoSum = function(nums, target) {
+//     let storage = new Map();
+//     for (let i = 0; i < nums.length; i++) {
+//         const element = target - nums[i];
+//     if(storage.has(element)) {
+//         return [storage.get(element), i]
+//     }
+//     storage.set(nums[i], i)        
+//     }
+//     return [];
+// }
+
+// const result = twoSum(nums, target)
+
+// console.log(result)
+
+const twoSum = (nums, target) => {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const element = target - nums[i];
+        if (map.has(element)) {
+            return [map.get(element), i]
+        }
+        map.set(nums[i], i)
+    }
+    return []
 }
-numberMap.set(nums[index], index)   
-}
 
-return [];
-
-}
-
-let result = leetCodeOne(nums, target)
-
-console.log('indeces', result)
+console.log(twoSum(nums, target))
