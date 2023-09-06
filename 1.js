7b38602e52215ab28acc47a5561a3ddd18f1f1c2
escotoj <j.escoto14@gmail.com>
2023-09-06 16:26:53 -0700

    walk through in english and then solve

--- 1.js
+++ 1.js
@@ -10,7 +10,7 @@
 
 
 let nums = [2, 7, 11, 15, 11, 15, 12, 2, 12, 3, 4, 7, 12, 20];
-let target = 24;
+let target = 9;
 // function leetCodeOne(nums, target) {
 //     const numberMap = new Map();
 // for (let index = 0; index < nums.length; index++) {
@@ -63,16 +63,40 @@ let target = 24;
 
 // console.log(result)
 
-const twoSum = (nums, target) => {
-    let map = new Map();
+// const twoSum = (nums, target) => {
+//     let map = new Map();
+//     for (let i = 0; i < nums.length; i++) {
+//         const element = target - nums[i];
+//         if (map.has(element)) {
+//             return [map.get(element), i]
+//         }
+//         map.set(nums[i], i)
+//     }
+//     return []
+// }
+
+// console.log(twoSum(nums, target))
+
+function twoSum(nums, target) {
+    let numMap = new Map();
     for (let i = 0; i < nums.length; i++) {
-        const element = target - nums[i];
-        if (map.has(element)) {
-            return [map.get(element), i]
+        const compliment = target - nums[i];
+        if(numMap.has(compliment)) {
+            return [numMap.get(compliment), i]
         }
-        map.set(nums[i], i)
+        numMap.set(nums[i], i)
     }
     return []
 }
 
 console.log(twoSum(nums, target))
+
+// 09/06 everything was set up correctly expect the return. 
+//needs more work
+// declare a function and pass the two parameter, nums, and target. 
+// begin the function byy declaring a changing variable called numMap and have it equal to a new Map in order to keep track of indices. 
+// we need a for-loop to go through the array and find the compliment to each number. 
+// we have a varibel insdie the loop that calbulates the differnece between nums[i] and target. 
+// Inside the loop we create conditional statements that will check if the map has the compliment for each number in the array. 
+// if the map contains the compliment we will return it as an arra that contains the compliment and the index of that number.
+// 
--- 121.js
+++ 121.js
@@ -13,20 +13,50 @@
 
 const prices = [7,1,5,3,6,4]; 
 
-function maxProfit (prices) {
-    let maximumGain = 0
-    let minimalGain = prices[0]
-    for (let i = 0; i < prices.length; i++) {
-        if(prices[i] < minimalGain) {
-            minimalGain = prices[i];
-        } else if (maximumGain < prices[i] - minimalGain) {
-            maximumGain = prices[i]
-        }
+// function maxProfit (prices) {
+//     let maximumGain = 0
+//     let minimalGain = prices[0]
+//     for (let i = 0; i < prices.length; i++) {
+//         if(prices[i] < minimalGain) {
+//             minimalGain = prices[i];
+//         } else if (maximumGain < prices[i] - minimalGain) {
+//             maximumGain = prices[i]
+//         }
         
+//     }
+//     return maximumGain;
+// }
+
+// console.log(maxProfit(prices))
+
+// 09/05 - structure was right but if statements were incorrect order
+// ================================================
+// ================================================
+
+// declare the function, and pass the nums parameter
+// initiate bestProfit variable by having it equal 0 as a form of Storage for later use.
+// initiate minProfit variable by having it equal to the first number in nums like nums[0].
+// being a for-loop to iterate through each number. 
+// inside this for-loop we have conditional statements where set that if nums[0] is less than minProfit, we have minprofit equal that number as an update.
+// we set up a second condition using an else if, so that it only checks one condition at a time
+// in the second condition we set it so that if bestProfit is less than nums[i] - minprofit, we set bestprofit to equal that price[i]
+// return the bestprofit
+
+const maxProfit = function (prices) {
+    let bestProfit = 0;
+    let littleProfit = prices[0];
+    for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < littleProfit) {
+        littleProfit = prices[i];
+    } else if (bestProfit < prices[i] - littleProfit) {
+        bestProfit = prices[i]
+    }   
     }
-    return maximumGain;
+    return bestProfit
 }
 
 console.log(maxProfit(prices))
 
-// 09/05 - structure was right but if statements were incorrect order
+// 09/06 - wrote it out in english first and in JS for the right answer first try. 
+// ================================================================================================
+// ================================================================================================
