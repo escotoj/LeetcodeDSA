483277f3e641ba052a748846b68f15de6f70d684
escotoj <j.escoto14@gmail.com>
2023-09-18 16:58:07 -0700

    titleCase solved

--- titleCase.js
+++ titleCase.js
@@ -1,12 +1,19 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
 var titleCase = function(str) {
-    let newStr = ""
-    for (let i = 0; i < str; i++) {
-        const element = str[i];
-        console.log(element)
+    let length = str.length
+    const element = str.split(' ');
+    let newStr = [ ];
+    // console.log(str)
+    for (let i = 0; i < length; i++) {
+        let word = element[i];
+        let firstLetter = word.charAt(0).toUpperCase();
+        let otherLetters = word.slice(1).toLowerCase()
+        let capWord = firstLetter + otherLetters;
+        newStr.push(capWord);
         
+        console.log(capWord)
     }
+    let results = newStr.join(" ")
+    return results
 };
-
-console.log(titleCase())
