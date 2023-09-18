ab13fa96fd107ef73bf4b75ad63b42283ac1adb3
escotoj <j.escoto14@gmail.com>
2023-09-17 20:43:32 -0700

    string manipulation to capital case first letter

---
+++ titleCase.js
@@ -0,0 +1,12 @@
+// Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
+
+var titleCase = function(str) {
+    let newStr = ""
+    for (let i = 0; i < str; i++) {
+        const element = str[i];
+        console.log(element)
+        
+    }
+};
+
+console.log(titleCase())
