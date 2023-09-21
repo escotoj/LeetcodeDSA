bc0be8d4d06e73601360474c2099d2ea3d81e52c
escotoj <j.escoto14@gmail.com>
2023-09-20 17:07:47 -0700

    round two of 1108 replacing a character, string manupulation

--- 1108.js
+++ 1108.js
@@ -1,4 +1,49 @@
+// 1108. Defanging an IP Address
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+
+// Example 1:
 const address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+// Example 2:
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+// name function
+// since you are altering an exciting string, you initiate a variable to an empty string
+// create a variable for . 
+// one for [.]
+//  use a for-loop and break down each character
+// now an if statement that checks for a boolean that is true, we add that character to the empty string we created
+// if false we add that chracter to the string and outside the for loop we call the new string variable
+
+const ipDefang = function(address) {
+    let newAddress = '';
+    let char = '.'
+    let newChar = '[.]'
+    for (let i = 0; i < address.length; i++) {
+  if (address[i] === char) {
+    newAddress += newChar
+  } else {
+    newAddress += address[i]
+  }   
+    }
+    return newAddress
+
+}
+
+console.log(ipDefang(address))
+
+
+
+
+
+
+
+
+// const address = "1.1.1.1"
 
 //FIRST ATTEMPT 09/13, stuck at for-loop, needed an new empty string to add newchar
 // var defangIPaddr = function(address) {
@@ -21,20 +66,20 @@ const address = "1.1.1.1"
 
 
 // ANSWER
-var defangIPaddr = function(address) {
-    let newAddress = ''
-    let char = '.'
-    let newChar = '[.]'
-    for(let i = 0; i < address.length; i++) {
-        if (address[i] === char) {
-           newAddress += newChar
-        } else {
-            newAddress += address[i]
-        }
-    }
-        console.log(newAddress)
-    return newAddress
-};
+// var defangIPaddr = function(address) {
+//     let newAddress = ''
+//     let char = '.'
+//     let newChar = '[.]'
+//     for(let i = 0; i < address.length; i++) {
+//         if (address[i] === char) {
+//            newAddress += newChar
+//         } else {
+//             newAddress += address[i]
+//         }
+//     }
+//         console.log(newAddress)
+//     return newAddress
+// };
 
 
 
@@ -60,4 +105,4 @@ var defangIPaddr = function(address) {
 
 
 
-defangIPaddr(address)
+// defangIPaddr(address)
