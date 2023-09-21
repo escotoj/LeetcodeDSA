6903e27e818afefde604353380034ad944ebeaa7
escotoj <j.escoto14@gmail.com>
2023-09-21 10:54:38 -0700

    alternative solution for 1512 using soft and two for-loops

--- 1512.js
+++ 1512.js
@@ -42,3 +42,26 @@ var numIdenticalPairs = function(nums) {
 
     return result
 }
+
+// SOLUTION USING SORT
+// function numIdenticalPairs(nums) {
+//     let i=0
+//     let j=i+1
+//     let count=0
+//     nums.sort()
+//     while(i<j){
+//         if(nums[i]===nums[j]){
+//             count++;
+//             j++
+//         }
+//         else{
+//             i++
+//             j=i+1
+//             if(nums[j]===undefined)
+//                 {
+//                     return count
+//                 }
+//         }
+//     }
+//     return count
+// };
