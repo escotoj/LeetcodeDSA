2b08d102205bce641162a27f5de1b4ef42b51840
escotoj <j.escoto14@gmail.com>
2023-09-26 12:29:54 -0700

    solution to 1678, turn string to array and use for-loop

---
+++ 1678.js
@@ -0,0 +1,47 @@
+// 1678. Goal Parser Interpretation
+
+// Companies
+// You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+// Given the string command, return the Goal Parser's interpretation of command.
+
+ 
+
+// Example 1:
+// const command = "G()(al)"
+// Output: "Goal"
+// Explanation: The Goal Parser interprets the command as follows:
+// G -> G
+// () -> o
+// (al) -> al
+// The final concatenated result is "Goal".
+
+// Example 2:
+
+const command = "G()()()()(al)"
+// Output: "Gooooal"
+// Example 3:
+
+// Input: command = "(al)G(al)()()G"
+// Output: "alGalooG"
+
+var interpret = function(command) {
+    let word = ''
+    let split = command.split('(')
+    console.log(split)
+    for (let i = 0; i < split.length; i++) {
+        console.log('el', split[i])
+        if(split[i] === "G") {
+            word += split[i];
+        } else if (split[i] === "al)") {
+            word += "al";
+        } else if (split[i] === ')') {
+            word += 'o'
+        }
+    }
+    console.log(word)
+    return word
+    
+};
+
+interpret(command)
