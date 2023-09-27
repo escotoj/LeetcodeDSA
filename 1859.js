2678a0d96ce1c5d4a03c9062d50c4526d2920529
escotoj <j.escoto14@gmail.com>
2023-09-27 10:28:12 -0700

    final solution for 1859, passed

--- 1859.js
+++ 1859.js
@@ -12,7 +12,7 @@ const s = "is2 sentence4 This1 a3"
 // Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
 
 // Example 2:
-// Input: s = "Myself2 Me1 I4 and3"
+// const s = "Myself2 Me1 I4 and3"
 // Output: "Me Myself and I"
 // Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.
 
@@ -27,40 +27,114 @@ const s = "is2 sentence4 This1 a3"
 
 
 
+//FIRST ATTEMPT 09/25 passed one test case. 
+// var sortSentence = function(s) {
+//     let stack = []
+//     let split = s.split(' ')
+//     console.log(split)
+//     for (let i = 0; i < split.length; i++) {
+//         if(split[i].includes('1')) {
+//             stack.push(split[3])
+//         } else if(split[i].includes('2')) {
+//             stack.push(split[2])
+//         } else if(split[i].includes('3')) {
+//             stack.push(split[1])
+//         } else if (split[i].includes('4')) {
+//             stack.push(split[0])
+//         }
+//     }
+//     let result = []
+//     console.log("newArray", stack)
+//     for (let j = 0; j < stack.length; j++) {
+//         const element = stack[j];
+//         if(element.includes('1')) {
+//             result.push(stack[j].split(1))
+//         } else if (element.includes('2')) {
+//             result.push(stack[j].split(2))
+//         } else if (element.includes('3')) {
+//             result.push(stack[j].split(3))
+//         } else if (element.includes('4')) {
+//             result.push(stack[j].split(4))
+//         } 
+//     }
+//     let newResult = result.join('')
+//     let finalResult = newResult.split(",").join(" ")
+//     console.log(finalResult)
+//     return finalResult
 
+// };
+// sortSentence(s)
+
+
+// CHATGPT assistance but still but failed 15/45 cases. 
+// var sortSentence = function(s) {
+//     let stack = [];
+//     let split = s.split(' ');
+
+//     // First loop: Identify the positions based on numbers and push words into stack
+//     for (let i = 0; i < split.length; i++) {
+//         if (split[i].includes('1')) {
+//             stack[0] = split[i].replace(/\d/g, ''); // Remove the number and store in the correct position
+//         } else if (split[i].includes('2')) {
+//             stack[1] = split[i].replace(/\d/g, '');
+//         } else if (split[i].includes('3')) {
+//             stack[2] = split[i].replace(/\d/g, '');
+//         } else if (split[i].includes('4')) {
+//             stack[3] = split[i].replace(/\d/g, '');
+//         }
+//     }
+
+//     let result = [];
+// console.log(stack.join(' '))
+//     // Second loop: Extract words without numbers and store in result
+//     for (let j = 0; j < stack.length; j++) {
+//         const element = stack[j];
+//         console.log(element)
+//         if (element) {
+//             result.push(element);
+//         }
+//     }
+
+//     // Join the words in the result array with spaces to get the final result
+//     let finalResult = result.join(' ');
+
+//     return finalResult;
+// };
+
+// const s = "Myself2 Me1 I4 and3";
+
+
+// FINAL SOLUTION - passed all testcases 
 var sortSentence = function(s) {
-    let stack = []
-    let split = s.split(' ')
-    console.log(split)
-    for (let i = 0; i < split.length; i++) {
+    let stack = [];
+    let split = s.split(' ');
+    for(let i = 0 ; i < split.length; i++) {
         if(split[i].includes('1')) {
-            stack.push(split[3])
-        } else if(split[i].includes('2')) {
-            stack.push(split[2])
-        } else if(split[i].includes('3')) {
-            stack.push(split[1])
+            stack[0] = split[i].replace(/\d/g, '');
+        } else if (split[i].includes('2')) {
+            stack[1] = split[i].replace(/\d/g, '');
+        } else if (split[i].includes('3')) {
+            stack[2] = split[i].replace(/\d/g, '');
         } else if (split[i].includes('4')) {
-            stack.push(split[0])
+            stack[3] = split[i].replace(/\d/g, '');
+        } else if (split[i].includes('5')) {
+            stack[4] = split[i].replace(/\d/g, '');
+        } else if (split[i].includes('6')) {
+            stack[5] = split[i].replace(/\d/g, '');
+        } else if (split[i].includes('7')) {
+            stack[6] = split[i].replace(/\d/g, '');
+        } else if (split[i].includes('8')) {
+            stack[7] = split[i].replace(/\d/g, '');
+        }else if (split[i].includes('9')) {
+            stack[8] = split[i].replace(/\d/g, '');
+        }else if (split[i].includes('10')) {
+            stack[9] = split[i].replace(/\d/g, '');
         }
     }
-    let result = []
-    console.log("newArray", stack)
-    for (let j = 0; j < stack.length; j++) {
-        const element = stack[j];
-        if(element.includes('1')) {
-            result.push(stack[j].split(1))
-        } else if (element.includes('2')) {
-            result.push(stack[j].split(2))
-        } else if (element.includes('3')) {
-            result.push(stack[j].split(3))
-        } else if (element.includes('4')) {
-            result.push(stack[j].split(4))
-        } 
-    }
-    let newResult = result.join('')
-    let finalResult = newResult.split(",").join(" ")
-    console.log(finalResult)
-    return finalResult
+    let result = stack.join(' ');
 
+    return result
 };
-sortSentence(s)
+
+const output = sortSentence(s);
+console.log(output);
