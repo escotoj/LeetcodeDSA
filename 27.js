4437780d36d618949f5b56bc9fa97cc50d6c65ef
escotoj <j.escoto14@gmail.com>
2023-10-18 17:28:34 -0700

    updates to the answer for removingelement

--- 27.js
+++ 27.js
@@ -55,7 +55,7 @@ function removeElement(nums, val) {
   let k = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== val) {
-      nums[k] = nums[i]; // not necessarily needed
+      nums[k] = nums[i]; 
       k++;
     }
   }
