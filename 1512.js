3231ae822b4948ccd145a4cad0e86c30d3578b2c
escotoj <j.escoto14@gmail.com>
2024-01-12 15:46:28 -0800

    another solution for good pair problem to study but long run time using nested forloop

--- 1512.js
+++ 1512.js
@@ -65,3 +65,17 @@ var numIdenticalPairs = function(nums) {
 //     }
 //     return count
 // };
+
+// SOLUTION USING NESTED LOOP
+// supposedly it would'nt be the best solution since the run time is long and would fail interview 
+var numIdenticalPairs = function(nums) {
+    let count = 0
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j <nums.length; j++) {
+            if(nums[i] === nums[j]) {
+                count++
+            }
+        }
+    }
+      return count  
+    };
