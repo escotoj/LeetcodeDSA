8e57a67792c35ecd60ddbd00a065bb351a41a5c8
escotoj <j.escoto14@gmail.com>
2024-01-26 11:54:24 -0800

    modified solution by removing methods that take up time and space, plus additional solution that uses filter and substring

--- 2678.js
+++ 2678.js
@@ -36,3 +36,24 @@ count++
     }
     return count
 };
+
+// Altered version with a better run time by eliminating the use of two loops and removing variables, like num and the use of parseInt and having the age array be in the loop
+var countSeniors = function (details) {
+    let count = 0
+    for (let i = 0; i < details.length; i++) {
+        let age = []
+        let dig1 = [details[i].charAt(11), details[i].charAt(12)].join('')
+        age.push(dig1)
+        if (age > 60) {
+            count++
+        }
+    }
+
+    return count
+};
+
+
+//Solution using filter to apply function to each el and using substring to find the digits that are less than target
+var countSeniors = function(details) {
+    return details.filter(detail => parseInt(detail.substring(11,13)) > 60).length;
+};
